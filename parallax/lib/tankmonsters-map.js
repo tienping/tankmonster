d8e525ca9850f84aaae1ade064e0465baa08459a
Tien Ping <tplim@hermo.my>
2017-08-01 23:43:36 +0800

    edit

--- parallax/lib/tankmonsters-map.js
+++ parallax/lib/tankmonsters-map.js
@@ -1,16 +1,27 @@
 function myMap() {
-    myCenter=new google.maps.LatLng(41.878114, -87.629798);
-    var mapOptions= {
-        center:myCenter,
-        zoom:12, scrollwheel: false, draggable: false,
-        mapTypeId:google.maps.MapTypeId.ROADMAP
-    };
-    var map=new google.maps.Map(document.getElementById("googleMap"),mapOptions);
+    var pinColor = "FE7569";
+    var pinImage = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + pinColor,
+        new google.maps.Size(21, 34),
+        new google.maps.Point(0,0),
+        new google.maps.Point(10, 34));
+    var pinShadow = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_shadow",
+        new google.maps.Size(40, 37),
+        new google.maps.Point(0, 0),
+        new google.maps.Point(12, 35));
 
+    myCenter=new google.maps.LatLng(1.5820093,103.7189516);
+    var mapOptions= {
+        center: myCenter,
+        zoom: 12
+    };
+    
+    var map=new google.maps.Map(document.getElementById("googleMap"),mapOptions);
     var marker = new google.maps.Marker({
         position: myCenter,
+        map: map,
+        icon: pinImage,
+        shadow: pinShadow
     });
-    marker.setMap(map);
 }
 
 // Modal Image Gallery
